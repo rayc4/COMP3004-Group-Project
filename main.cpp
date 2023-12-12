1767efc7c95c10bea83a19efc7946ec86993e1d3
nicozobnin <nicozobnin10b@gmail.com>
2023-12-11 20:39:11 -0500

    Testing building project
    added logic to main to be able to test multiple isntances of our project
    at once
    
    over time, we have had multiple/different issues where our project
    wouldnt build, without a reliable source of issue
    
    this logic on main will allow us to test multiple instances and see if
    any of them crashes on launch, or if they fail to launch, and thus be
    able to decypher if there are any issues

--- main.cpp
+++ main.cpp
@@ -4,30 +4,69 @@
 #include "aed.h"
 
 #include <QApplication>
+#include <QDebug>
+#include <vector>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    //implemented testing code
+    //this will launch multiple instances
+    //to be able to test if our project will always compile correctly, and not by coincidence
 
-    //mainwindow creates AED
-    //mainwidnow has AED getter
+    const int numberOfInstances = 10;
+    // Set the number of instances here
+    //set 1 if not testing
+    std::vector<MainWindow*> windows;
+    std::vector<PatientWindow*> patientWindows;
+    int initializedWindows = 0;
 
-    //patientwindow is created, pass mainwindow getter through connt argument
+    for (int i = 0; i < numberOfInstances; ++i) {
+        try {
+            MainWindow *w = new MainWindow(nullptr);
+            windows.push_back(w);
+            w->show();
+            qDebug() << "MainWindow instance" << i + 1 << "initialized";
 
-    MainWindow *w = new MainWindow(nullptr);
-    w->show();
+            PatientWindow *patientW = new PatientWindow(w);
+            patientWindows.push_back(patientW);
+            patientW->show();
+            qDebug() << "PatientWindow instance" << i + 1 << "initialized";
 
-    PatientWindow *patientW = new PatientWindow(w);
-    patientW->show();
+            w->setPatientWindow(patientW);
+            w->initPatient();
 
-    w->setPatientWindow(patientW);
-    w->initPatient();
+            initializedWindows += 2;
+            // gotta use base 2
+            //because we are accounting for
+            //for MainWindow and PatientWindow
+        } catch (const std::exception& e) {
+            qDebug() << "Exception occurred while creating window instance:" << e.what();
+        }
+    }
 
-    //This does not work at the moment:
-    //delete patient;
-    //delete patientW;
-    //delete aed;
+    int result = a.exec();
 
-    return a.exec();
+    // Check if all windows were initialize
+    if (initializedWindows == numberOfInstances * 2)
+    {
+        qDebug() << "All windows initialized successfully.";
+    }
+    else
+    {
+        qDebug() << "Some windows failed to initialize.";
+    }
+
+    // Clean up
+    for (auto *window : windows)
+    {
+        delete window;
+    }
+    for (auto *patientWindow : patientWindows)
+    {
+        delete patientWindow;
+    }
+    //easier using for auto
+    return result;
 }
